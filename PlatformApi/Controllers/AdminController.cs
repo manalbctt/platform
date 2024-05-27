ababa4936ab20eeb33ce28324148ac65f52039f9
AYOUB BOUSSAIDI <Boussaidi-Ayoub@hotmail.com>
2024-05-27 03:31:53 +0100

    add signalR

--- PlatformApi/Controllers/AdminController.cs
+++ PlatformApi/Controllers/AdminController.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 using PlatformApi.Dtos.Request;
 using PlatformApi.Helper.Jwt;
+using PlatformApi.Hubs;
 using PlatformApi.Models;
 using PlatformApi.Services.Contract;
 
@@ -15,12 +17,15 @@ namespace PlatformApi.Controllers
         private readonly IAdminService _adminService;
         private readonly IMapper _mapper;
         private readonly JwtHelper _jwtHelper;
+        private readonly IHubContext<userHub> _hubContext;
 
-        public AdminController(IAdminService adminService , IMapper mapper, JwtHelper jwtHelper)
+
+        public AdminController(IAdminService adminService , IMapper mapper, JwtHelper jwtHelper,IHubContext<userHub> hubContext)
         {
             this._adminService = adminService;
             this._mapper = mapper;
             this._jwtHelper= jwtHelper;
+            this._hubContext= hubContext;
         }
 
         [HttpPost]
@@ -49,6 +54,9 @@ namespace PlatformApi.Controllers
             await this._adminService.validerVendeur(adminVendeur.VendeurId);
 
             await this._adminService.updateVendeurAdmin(adminVendeur);
+
+
+            await _hubContext.Clients.All.SendAsync("UserValidated", adminVendeur.VendeurId);
             
 
 
---
+++ PlatformApi/Hubs/userHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace PlatformApi.Hubs
+{
+    public class userHub :Hub
+    {
+        public async Task Negotiate(int userId)
+        {
+            await Clients.All.SendAsync("UserValidated", userId);
+        }
+    }
+}
